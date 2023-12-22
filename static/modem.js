41aa283c434af4d6d66021a5a6b6103d485d8714
adrian154 <94568a@gmail.com>
2023-12-21 17:30:48 -0800

    allow device selection

--- static/modem.js
+++ static/modem.js
@@ -34,20 +34,24 @@ const makeRRCFilter = (symbolLen, rolloff) => {
 const audioCtx = new AudioContext();
 const rrcFilter = makeRRCFilter(modulationSettings.symbolLen, modulationSettings.rrcRolloff);
 
-let loopback = false,
-    output = null;
+const loopbackCheckbox = document.getElementById("loopback");
+let output = null;
 
 document.getElementById("start-transmit").addEventListener("click", async event => {
     
     // disable button 
     event.target.disabled = 1;
+    outputSelect.disabled = 1;
 
     // create transmitter worklet and start audio context
     await audioCtx.audioWorklet.addModule("tx.js");
     const transmitter = new AudioWorkletNode(audioCtx, "modem-transmitter", {processorOptions: {modulationSettings, rrcFilter}});
-    if(loopback) {
+    if(loopbackCheckbox.checked) {
         output = transmitter;
     } else {
+        if(audioCtx.setSinkId) {
+            audioCtx.setSinkId(outputSelect.value);
+        }
         transmitter.connect(audioCtx.destination);
     }
     audioCtx.resume();
@@ -92,7 +96,7 @@ const drawConstellation = receiver => {
     let points = [];
 
     receiver.port.onmessage = (message) => {
-        if(count % 20 == 0) {
+        if(count % 50 == 0) {
             ctx.fillStyle = "#ffffff";
             ctx.fillRect(0, 0, canvas.width, canvas.height);
             ctx.fillStyle = "#000000";
@@ -114,12 +118,18 @@ const drawConstellation = receiver => {
 
 document.getElementById("start-receive").addEventListener("click", async event => {
 
+    if(loopbackCheckbox.checked && !output) {
+        alert("You need to start the transmitter first!");
+        return;
+    }
+
     // disable button 
     event.target.disabled = 1;
+    inputSelect.disabled = 1;
 
     await audioCtx.audioWorklet.addModule("rx.js");
     const receiver = new AudioWorkletNode(audioCtx, "modem-receiver", {processorOptions: {modulationSettings, rrcFilter}});
-    const input = loopback ? output : await audioCtx.createMediaStreamSource(await navigator.mediaDevices.getUserMedia({audio: true}));
+    const input = loopbackCheckbox.checked ? output : await audioCtx.createMediaStreamSource(await navigator.mediaDevices.getUserMedia({audio: {deviceId: inputSelect.value}}));
     input.connect(receiver);
     audioCtx.resume();
 
@@ -130,3 +140,32 @@ document.getElementById("start-receive").addEventListener("click", async event =
     document.getElementById("delay").addEventListener("input", event => receiver.parameters.get("delay").value = event.target.value / 100 * modulationSettings.symbolLen);
 
 });
+
+// populate input device options
+const inputSelect = document.getElementById("input-select"),
+      outputSelect = document.getElementById("output-select");
+
+navigator.mediaDevices.getUserMedia({audio: true}).then(() => {
+    navigator.mediaDevices.enumerateDevices().then(devices => devices.forEach(device => {
+        if(device.kind == "audioinput" || device.kind == "audiooutput") {
+            const option = document.createElement("option");
+            option.value = device.deviceId;
+            option.textContent = device.label;
+            option.selected = 1;
+            if(device.kind == "audioinput")
+                inputSelect.append(option);
+            else if(audioCtx.setSinkId)
+                outputSelect.append(option);
+        }
+    }))
+});
+
+if(!audioCtx.setSinkId) {
+    outputSelect.disabled = 1;
+    const option = document.createElement("option");
+    option.textContent = "your browser does not support output selection :(";
+    option.selected = 1;
+    outputSelect.append(option);
+}
+
+// disable input/output selection if loopback checkbox is checked 
